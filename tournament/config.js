634b0112fde96e095895ff1919c860897f8024d7
YoonSik Lim <57971757+AmSoMad@users.noreply.github.com>
2025-08-19 02:12:14 +0900

    Delete tournament directory

--- tournament/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://atjxeiquqhihykpxiaxt.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0anhlaXF1cWhpaHlrcHhpYXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwMDc4NjMsImV4cCI6MjA3MDU4Mzg2M30.kgxY_1meq10y6lhI-wQhX5QPbfyXk4eIkp1o-iJ1Bpk"
-};
